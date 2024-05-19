bf1fce4f66856f48337ef50f9419183ae5df9197
miqueiasbelfort <kawasousa1554@gmail.com>
2024-05-19 11:18:35 -0300

    database delete with yours tables

--- comands/delete.js
+++ comands/delete.js
@@ -23,13 +23,14 @@ const deleteCommand = (com, database) => {
             // Verificar se tem tabelas dentro do banco
             // SE tiver tabelas peça permisão para excluir tudo
             // SENÂO apenas exclua o banco
+            deleteDatabase(choise, force);
             break;
         case "tabela":
             // EXCLUIR UMA TABELA
             // Verificar se tem dados salvos
             // SE tiver dados, peça permissão
             // SENÂO apenas delete a tabela
-            deleteDatabase(choise, database, force);
+            deleteTable(choise, database, force);
             break;
         case "--ajuda":
             console.log("\nCRIAR BANCO [nome] - Criar um novo banco de dados;\nCRIAR TABELA [nome] (<chave>:tipo-tipo) - Criar uma tabela no banco;".gray);
@@ -39,14 +40,57 @@ const deleteCommand = (com, database) => {
     };
 };
 
-const deleteDatabase = (tableName, db, isForce) => {
+const deleteDatabase = (name, force) => {
+    const dbPath = path.join(__dirname, "..", "databases", name);
+    fs.readdir(dbPath, {withFileTypes: true}, (err, entries) => {
+        if(err){
+            console.log("[ERRO]: Houve um erro ao tentar apagar banco.".red, err);
+            return;
+        }
+        
+        let pending = entries.length;
+        if(pending == 0 && force == "-F"){
+            fs.rmdir(dbPath);
+            console.log("[SUCESSO]: Banco deletado.");
+            return;
+        }
+
+        if(force !== "-F"){
+            console.log("[INF COMANDO]: Para excluir use DELETAR TABELA <nome> -F".cyan);
+            return;
+        }
+
+        entries.forEach(entry => {
+            const filePath = path.join(dbPath, entry.name);
+            fs.unlink(filePath, err => {
+                if(err){
+                    console.log("[ERRO]: Não foi possivel excluir as tabelas.".red, err);
+                    return;
+                }
+                if(!--pending){
+                    fs.rmdir(dbPath, err => {
+                        if(err){
+                            console.log("[ERRO]: Erro ao tentar apagar o banco.".red, err);
+                            return;
+                        }
+                    });
+                }
+            });
+        });
+
+        console.log(`[SUCESSO]: O banco de dados ${name} foi deletado.`.green);
+    });
+    
+};
+
+const deleteTable = (tableName, db, isForce) => {
     
     const tablePath = path.join(__dirname, "..", "databases", db, `${tableName}.csv`);
     
     if(!fs.existsSync(tablePath)){
         console.log(`[NÂO ACEITO]: Não existe tabela com o nome "${tableName}" no banco de dados "${db}".`.yellow);
         return;
-    }
+    };
 
     const tableContent = fs.readFileSync(tablePath, 'utf8');
 
@@ -62,10 +106,9 @@ const deleteDatabase = (tableName, db, isForce) => {
             console.log(`[SUCESSO]: A tabela ${tableName} foi deletada.`.green);
         });
         return;
-    }
+    };
 
     console.log("[INF COMANDO]: Para excluir use DELETAR TABELA <nome> -F".cyan);
-    return;
-}
+};
 
 module.exports = deleteCommand;
